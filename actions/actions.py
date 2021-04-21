62483e148309740f3ab3bff6ee6c5c9a46503156
Poornartha Sawant <spoornartha@gmail.com>
2021-04-22 00:05:21 +0530

    Intial Setup

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,64 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
+
+
+class AskSlot(Action):
+
+    def name(self) -> Text:
+        return "ask_slot"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello, will you be on 26th April at 12 PM?")
+
+        return []
+
+
+class SlotAccepted(Action):
+
+    def name(self) -> Text:
+        return "accept_slot"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+
+        dispatcher.utter_message(text="Thank you for accepting the slot.")
+
+        return []
+
+
+class SlotRejected(Action):
+
+    def name(self) -> Text:
+        return "reject_slot"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="The Slot has been rejected.")
+
+        return []
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
 Chatbot for Interview Scheduling
 
+rasa train
+rasa run actions 
+rasa shell --endpoints endpoints.yml
+
 Authors:
 1. Poornartha Sawant
 2. Rajat Joshi
