35b6d79f9c464fe5bba4d0f5d75f348cec3912bc
Shubh Sanghvi <shubhsanghvi242@gmail.com>
2021-05-01 17:46:50 +0530

    action changes

--- actions/actions.py
+++ actions/actions.py
@@ -7,11 +7,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-from typing import Any, Text, Dict, List , Union
+from typing import Any, Text, Dict, List
 from datetime import datetime
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
 
 class ActionHelloWorld(Action):
 
@@ -27,10 +26,10 @@ class ActionHelloWorld(Action):
         return []
 
 
-class AskSlot(Action):
+class InsertInformation(Action):
 
     def name(self) -> Text:
-        return "ask_slot"
+        return "insert_information"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -40,10 +39,22 @@ class AskSlot(Action):
         print('This is the slot',tracker.slots)
         print('Username:',tracker.get_slot("username"))
         print('Empid:',tracker.get_slot("empid"))
+        print('Sender Id is:', tracker.sender_id)
         dispatcher.utter_message(text="Hello, will you be on 26th April at 12 PM?")
-
         return []
 
+class AskSlot(Action):
+
+    def name(self) -> Text:
+        return "ask_slot"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Would you be available on 2nd May 12pm!!")
+
+        return []
 
 class SlotAccepted(Action):
 
@@ -54,7 +65,7 @@ class SlotAccepted(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-
+        #   Just insert the accepted slot in database
         dispatcher.utter_message(text="Thank you for accepting the slot.")
 
         return []
@@ -66,6 +77,13 @@ class SlotRejected(Action):
         return "reject_slot"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        #   Check for given slot whether it is in free slots in database
+        #   if not in free slots then ask the same user about the slot
+        #   again repeat the procedure 
+        #   Confirm with other interviews about the slot
+        #   Revert back to the users for the slot
+
         d = tracker.latest_message['entities']
         t = d[0]
         v = t["value"]
